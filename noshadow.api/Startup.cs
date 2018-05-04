2b9d4ef77a28c35e4917e247a5e9f02bbe0c62a4
Guilherme de Paula <guidpt>
2018-05-04 10:26:13 -0300

    asdasd

--- noshadow.api/Startup.cs
+++ noshadow.api/Startup.cs
@@ -48,10 +48,7 @@ namespace noshadow.api
                 
             });
             
-            services.AddMvc(); services.Configure<MvcOptions>(options =>
-            {
-                options.Filters.Add(new CorsAuthorizationFilterFactory("AllowSpecificOrigin"));
-            });
+            services.AddMvc(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
